a6cfe7c523cd6d9b1cf16b9c55869dfa5900ae49
huni <huni@emmental.co.kr>
2021-11-19 21:28:10 +0900

    fix: 불필요한 파일 및 코드 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,46 +3,14 @@ import Head from 'next/head';
 import { CMS_NAME } from '../lib/constants';
 import Post from '../types/post';
 
-type Props = {
-  allPosts: Post[];
-};
-
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+const Index = () => {
   return (
     <>
       <Head>
         <title>Next.js Blog Example with {CMS_NAME}</title>
       </Head>
-      {/*{heroPost && (*/}
-      {/*    <div>*/}
-      {/*      {heroPost.title}*/}
-      {/*      {heroPost.coverImage}*/}
-      {/*      {heroPost.date}*/}
-      {/*      {heroPost.author}*/}
-      {/*      {heroPost.slug}*/}
-      {/*      {heroPost.excerpt}*/}
-      {/*    </div>*/}
-      {/*)}*/}
-      {/*{morePosts}*/}
     </>
   );
 };
 
 export default Index;
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt'
-  ]);
-
-  return {
-    props: { allPosts }
-  };
-};
