82b816187e4e49f740fd01e4147456bda003b396
huni <huni@emmental.co.kr>
2021-11-22 15:03:00 +0900

    feat: MainLayout에 ContentsArea 추가

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -1,9 +1,18 @@
+import { styled } from '../../stitches.config';
 import Header from './Header';
 
 export default function MainLayout() {
   return (
     <>
       <Header />
+      <ContentsArea></ContentsArea>
     </>
   );
 }
+
+const ContentsArea = styled('main', {
+  maxWidth: 1000,
+  width: '100%',
+  margin: '0 auto',
+  marginTop: 80
+});
