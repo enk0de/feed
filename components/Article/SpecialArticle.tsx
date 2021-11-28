c274ef4f865b1d244d95bbbb7d183482f1122a0a
huni <huni@emmental.co.kr>
2021-11-28 15:03:18 +0900

    feat: SpecialArticle에 empty 속성 추가 및 관련 로직 추가

--- components/Article/SpecialArticle.tsx
+++ components/Article/SpecialArticle.tsx
@@ -4,17 +4,26 @@ import SpecialArticleImage from './SpecialArticleImage';
 import SpecialArticleTitle from './SpecialArticleTitle';
 
 interface ISpecialArticleProps {
-  background?: ReactNode;
+  empty?: boolean;
   children?: ReactNode;
 }
 
-export default function SpecialArticle({ background, children }: ISpecialArticleProps) {
+export default function SpecialArticle({ empty, children }: ISpecialArticleProps) {
   return (
-    <a style={{ cursor: 'pointer' }}>
-      <SpecialArticleContainer>{children}</SpecialArticleContainer>
+    <a style={{ cursor: empty ? 'default' : 'pointer' }}>
+      <SpecialArticleContainer empty={!!empty}>
+        {empty ? <SpecialArticleEmpty /> : children}
+      </SpecialArticleContainer>
     </a>
   );
 }
+
+const SpecialArticleEmpty = styled('article', {
+  width: 250,
+  height: 150,
+  borderRadius: 15,
+  background: '$light2'
+});
 
 const SpecialArticleContainer = styled('article', {
   width: 250,
@@ -24,9 +33,15 @@ const SpecialArticleContainer = styled('article', {
   transitionProperty: 'transform,box-shadow',
   transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
   transitionDuration: '200ms',
-  '&:hover': {
-    transform: 'translateY(-5px)',
-    boxShadow: '$lightEl6'
+  variants: {
+    empty: {
+      false: {
+        '&:hover': {
+          transform: 'translateY(-5px)',
+          boxShadow: '$lightEl6'
+        }
+      }
+    }
   }
 });
 
