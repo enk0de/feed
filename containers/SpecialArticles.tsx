f7ca985c35224dfd319fb8ad81b09e9eb9e20ecf
huni <huni@emmental.co.kr>
2021-11-28 15:04:15 +0900

    feat: SpecialArticlesContainer 뷰포트 작아질 때 스크롤 가능하도록 구현

--- containers/SpecialArticles.tsx
+++ containers/SpecialArticles.tsx
@@ -6,19 +6,27 @@ export default function SpecialArticles() {
     <SpecialArticlesContainer>
       <SpecialArticle>
         <SpecialArticle.Image src="/assets/lol.jpeg" alt="롤" />
-        <SpecialArticle.Title>롤 중독자 임지훈</SpecialArticle.Title>
+        <SpecialArticle.Title>{`롤 중독자\n임지훈`}</SpecialArticle.Title>
       </SpecialArticle>
       <SpecialArticle>
         <SpecialArticle.Image src="/assets/toss.jpeg" alt="토스" />
-        <SpecialArticle.Title>토스 사랑해 사랑해</SpecialArticle.Title>
+        <SpecialArticle.Title>{`토스\n토스토스`}</SpecialArticle.Title>
       </SpecialArticle>
+      <SpecialArticle empty />
+      <SpecialArticle empty />
     </SpecialArticlesContainer>
   );
 }
 
 const SpecialArticlesContainer = styled('section', {
   display: 'flex',
+  overflow: 'scroll',
+  justifyContent: 'center',
+  '@media (max-width: 1140px)': {
+    justifyContent: 'flex-start'
+  },
   spaceX: '20px',
-  padding: '12px 40px',
-  width: '100%'
+  padding: '12px 40px 40px',
+  width: '100%',
+  boxSizing: 'border-box'
 });
