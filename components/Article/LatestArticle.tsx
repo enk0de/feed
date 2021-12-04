d7f894be8b46b6d22e0c8e0d69fb4070b497ccfb
huni <huni@emmental.co.kr>
2021-12-04 17:01:02 +0900

    feat: LatestArticle 컴포넌트 구현

---
+++ components/Article/LatestArticle.tsx
@@ -0,0 +1,53 @@
+import { styled } from '../../stitches.config';
+import { TypoHeadingH5, TypoLabel } from '../Common/Typography';
+
+interface ILatestArticleProps {
+  category: string;
+  createdAt: string;
+  title: string;
+}
+
+export default function LatestArticle({
+  category,
+  createdAt,
+  title
+}: ILatestArticleProps) {
+  return (
+    <a style={{ cursor: 'pointer' }}>
+      <LatestArticleContainer>
+        <DescriptionArea>
+          <TypoLabel type="large" css={{ color: '$dark3' }}>
+            {category}
+          </TypoLabel>
+          <Divider />
+          <TypoLabel type="large" css={{ color: '$dark3' }}>
+            {createdAt}
+          </TypoLabel>
+        </DescriptionArea>
+        <TypoHeadingH5>{title}</TypoHeadingH5>
+      </LatestArticleContainer>
+    </a>
+  );
+}
+
+const LatestArticleContainer = styled('article', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 4,
+  padding: '15px 0',
+  '&:hover h5': {
+    textDecoration: 'underline'
+  }
+});
+
+const Divider = styled('div', {
+  width: '1px',
+  height: '8px',
+  background: '$light0'
+});
+
+const DescriptionArea = styled('div', {
+  display: 'inline-flex',
+  alignItems: 'center',
+  spaceX: 8
+});
