87a739ef3ad5eec8934c398b55070bbee3fe442a
huni <huni@emmental.co.kr>
2021-12-05 16:11:57 +0900

    feat: 상세(Post) 페이지 레이아웃 컴포넌트 구현

---
+++ components/Layout/PostLayout.tsx
@@ -0,0 +1,56 @@
+import { ReactNode } from 'react';
+import { styled } from '../../stitches.config';
+import { TypoHeadingH1, TypoHeadingH3 } from '../Common/Typography';
+import Footer from './Footer';
+import Header from './Header';
+
+interface IPostMetadata {
+  title: string;
+}
+
+interface IPostLayoutProps {
+  meta: IPostMetadata;
+  children?: ReactNode;
+}
+
+export default function PostLayout({ meta, children }: IPostLayoutProps) {
+  return (
+    <>
+      <Header />
+      <PostLayoutContentsWrapper>
+        <Contents>
+          <TypoHeadingH1
+            css={{
+              textAlign: 'center',
+              fontSize: 28,
+              lineHeight: '38px',
+              '@bp2': {
+                fontSize: 40,
+                lineHeight: '54px'
+              }
+            }}
+          >
+            {meta?.title}
+          </TypoHeadingH1>
+          {children}
+        </Contents>
+      </PostLayoutContentsWrapper>
+      <Footer />
+    </>
+  );
+}
+
+const PostLayoutContentsWrapper = styled('main', {
+  marginTop: 64,
+  '@bp2': {
+    marginTop: 80
+  }
+});
+
+const Contents = styled('div', {
+  maxWidth: 1140,
+  width: '100%',
+  padding: '30px 40px',
+  margin: '0 auto',
+  boxSizing: 'border-box'
+});
