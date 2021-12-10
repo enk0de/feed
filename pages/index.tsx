0abcb244f0691fcec96959ef0cc5352e10ece600
hoondeveloper <hoonskyn@cau.ac.kr>
2021-12-10 21:38:13 +0900

    feat: index에서 전체 게시글 SSR에서 받아와 LatestArticles에 주입하도록 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,34 @@
+import { GetStaticProps } from 'next';
 import { ReactElement } from 'react';
 import MainLayout from '../components/Layout/MainLayout';
 import LatestArticles from '../containers/LatestArticles';
 import SpecialArticles from '../containers/SpecialArticles';
+import { getAllPosts } from '../lib/api';
+import PostType from '../types/post';
 
-const Index = () => {
+interface IIndexProps {
+  posts: PostType[];
+}
+
+const Index = ({ posts }: IIndexProps) => {
   return (
     <>
       <SpecialArticles />
-      <LatestArticles />
+      <LatestArticles posts={posts} />
     </>
   );
 };
 
 Index.getLayout = function (page: ReactElement) {
   return <MainLayout>{page}</MainLayout>;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = getAllPosts(['date', 'slug', 'title', 'category']);
+
+  return {
+    props: { posts }
+  };
 };
 
 export default Index;
