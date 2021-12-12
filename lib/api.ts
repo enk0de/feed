23ba7fca3c9b66a221eee91012f206fbcb396057
hoondeveloper <hoonskyn@cau.ac.kr>
2021-12-12 16:31:02 +0900

    feat: MDX 파일 다루는 API 로직 수정

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,8 @@
 import fs from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
-import PostType from '../types/post';
 
-const postsDirectory = join(process.cwd(), 'posts');
+const postsDirectory = join(process.cwd(), '_posts');
 
 export function getPostSlugs() {
   return fs.readdirSync(postsDirectory);
@@ -13,19 +12,24 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
   const realSlug = slug.replace(/\.mdx$/, '');
   const fullPath = join(postsDirectory, `${realSlug}.mdx`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
-  const { data } = matter(fileContents);
+  const { data, content } = matter(fileContents);
 
-  const item: Partial<PostType> = {};
+  const items: Record<string, any> = {};
 
   // Ensure only the minimal needed data is exposed
   fields.forEach((field) => {
     if (field === 'slug') {
-      item[field] = realSlug;
-    } else if (field === 'title' || field === 'date' || field === 'category')
-      item[field] = data[field];
+      items[field] = realSlug;
+    }
+    if (field === 'content') {
+      items[field] = content;
+    }
+    if (data[field]) {
+      items[field] = data[field];
+    }
   });
 
-  return item;
+  return items;
 }
 
 export function getAllPosts(fields: string[] = []) {
@@ -33,8 +37,6 @@ export function getAllPosts(fields: string[] = []) {
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
-    .sort((post1, post2) =>
-      post1.date && post2.date ? (post1.date > post2.date ? -1 : 1) : 0
-    );
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
 }
