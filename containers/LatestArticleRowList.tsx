972b80da95c289d736f46c5db1e9b282bbf57950
hoondeveloper <hoonskyn@cau.ac.kr>
2021-12-30 16:30:52 +0900

    feat: LatestArticleRowList 필터 기능 구현 (임시)

--- containers/LatestArticleRowList.tsx
+++ containers/LatestArticleRowList.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ComponentProps } from 'react';
 import LatestArticleRow from '../components/ArticleRow/LatestArticleRow';
+import Chip from '../components/Chip/Chip';
+import { IChipSetProps } from '../components/Chip/interface';
 import { TypoLabel } from '../components/Common/Typography';
 import { FRAME_PADDING_DEFAULT, FRAME_PADDING_MOBILE } from '../constants/paddings';
 import { styled } from '../stitches.config';
@@ -10,13 +14,31 @@ interface ILatestArticleRowListProps {
 }
 
 export default function LatestArticleRowList({ articles }: ILatestArticleRowListProps) {
+  const router = useRouter();
+  const category = (router.query?.category as string) ?? null;
+  const handleChipClick: IChipSetProps<string>['onChange'] = ({ value }) => {
+    if (value == null) {
+      router.push(router.basePath);
+    } else {
+      router.push(`${router.basePath}?category=${value}`);
+    }
+  };
+
   return (
     <LatestArticleRowListContainer>
-      <TypoLabel type="large" css={{ color: '$dark2' }}>
-        최신 아티클
-      </TypoLabel>
+      <HeaderArea>
+        <TypoLabel type="large" css={{ color: '$dark2' }}>
+          최신 아티클
+        </TypoLabel>
+        <Chip.Set value={category} onChange={handleChipClick}>
+          <Chip value={null}>전체</Chip>
+          <Chip disabled>하이</Chip>
+          <Chip value="story">하이</Chip>
+          <Chip value="77">하이</Chip>
+        </Chip.Set>
+      </HeaderArea>
       <div>
-        {articles?.map(({ slug, date, title, category }) => (
+        {articles.map(({ slug, date, title, category }) => (
           <Link href={`/articles/${category}/${slug}`} key={slug} passHref>
             <a>
               <LatestArticleRow title={title} category={category} createdAt={date} />
@@ -37,4 +59,10 @@ const LatestArticleRowListContainer = styled('section', {
     padding: `12px ${FRAME_PADDING_DEFAULT}px`
   },
   boxSizing: 'border-box'
+});
+
+const HeaderArea = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between'
 });
