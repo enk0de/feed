a267f47ddaa37583abb670463b777a97dc7e59bd
hoondeveloper <hoonskyn@cau.ac.kr>
2021-12-31 18:01:43 +0900

    feat: next.config redirects 이용하여 구 주소 마이그레이션 진행

--- next.config.js
+++ next.config.js
@@ -2,6 +2,35 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/
 });
 module.exports = withMDX({
+  async redirects() {
+    return [
+      {
+        source: '/angular/:path*',
+        destination: '/articles/angular/:path*',
+        permanent: true
+      },
+      {
+        source: '/essay/:path*',
+        destination: '/articles/essay/:path*',
+        permanent: true
+      },
+      {
+        source: '/story/:path*',
+        destination: '/articles/story/:path*',
+        permanent: true
+      },
+      {
+        source: '/web/:path*',
+        destination: '/articles/web/:path*',
+        permanent: true
+      },
+      {
+        source: '/webpack/:path*',
+        destination: '/articles/webpack/:path*',
+        permanent: true
+      }
+    ];
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   experimental: { workerThreads: true },
   webpack: (config, { isServer }) => {
