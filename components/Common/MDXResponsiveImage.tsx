4168047e4f923777b71f00c24ee63fc57cc63e40
hoondeveloper <hoonskyn@cau.ac.kr>
2022-01-01 22:48:54 +0900

    feat: MDXResponsiveImage Wrapper 구현

--- components/Common/MDXResponsiveImage.tsx
+++ components/Common/MDXResponsiveImage.tsx
@@ -1,20 +1,23 @@
 import { default as Image, ImageProps } from 'next/image';
-import { css } from '../../stitches.config';
+import { styled } from '../../stitches.config';
 
 export default function MDXResponsiveImage(props: ImageProps) {
   return (
-    <Image
-      {...props}
-      alt={props.alt}
-      layout="responsive"
-      loading="lazy"
-      quality="100"
-      className={ResponsiveImageStyle()}
-    />
+    <ImageWrapper>
+      <Image
+        {...props}
+        alt={props.alt}
+        layout="responsive"
+        loading="lazy"
+        quality="100"
+      />
+    </ImageWrapper>
   );
 }
 
-const ResponsiveImageStyle = css({
-  borderRadius: 10,
-  transition: '0.3s'
+const ImageWrapper = styled('span', {
+  '*': {
+    borderRadius: 10,
+    transition: '0.2s'
+  }
 });
