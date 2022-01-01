8a93b17c73ff3dc9b4068ed3bbef0554fc8663d5
hoondeveloper <hoonskyn@cau.ac.kr>
2022-01-01 16:36:53 +0900

    feat: CategorySlider 구현

---
+++ containers/CategorySlider.tsx
@@ -0,0 +1,26 @@
+import Chip from '../components/Chip/Chip';
+import { IChipSetProps } from '../components/Chip/interface';
+import HorizontalScrollShadower from '../components/Common/HorizontalScrollShadower';
+
+interface ICategorySliderProps {
+  categories: string[];
+  selected: string;
+  onChange: IChipSetProps<string>['onChange'];
+}
+
+export default function CategorySlider({
+  categories,
+  selected,
+  onChange
+}: ICategorySliderProps) {
+  return (
+    <Chip.Set value={selected} onChange={onChange}>
+      <Chip value={null}>전체</Chip>
+      {categories.map((category) => (
+        <Chip value={category} key={category}>
+          {category}
+        </Chip>
+      ))}
+    </Chip.Set>
+  );
+}
