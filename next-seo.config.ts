fe15e8fb0e789391bb4b2afa03647fde97ef3056
hoondeveloper <hoonskyn@cau.ac.kr>
2022-01-03 20:50:35 +0900

    fix: next-seo.config에 og:site_name 항목 제거

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -5,7 +5,6 @@ const SEO: NextSeoProps = {
     type: 'website',
     locale: 'ko_KR',
     url: 'https://feed.hoondev.com/',
-    site_name: '훈데브 피드',
     description: '소프트웨어 엔지니어링에 관한 다양한 이야기를 전합니다.'
   },
   titleTemplate: '%s | 훈데브 피드',
